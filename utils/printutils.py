73edf2c6d99ebd255b7ad2e85dd6cfbd876fbebb
Flavio <fmontesano@gmail.com>
2023-12-19 20:24:01 +0100

    radio updates

--- utils/printutils.py
+++ utils/printutils.py
@@ -1,9 +1,9 @@
 def hex_array_to_str(hex_array):
-    return f"[{','.join(map(lambda x: hex(x), hex_array))}]"
+    return f"[{','.join(map(lambda x: hex(x), hex_array))}]".upper()
 
 
 def hex_int_to_str(hex_int):
-    return f"{hex(hex_int)}"
+    return f"{(hex(hex_int))}".upper()
 
 
 def hex_n_array_to_str(hex_array):
