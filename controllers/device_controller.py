afd7749f65ac0734dbe16380660dbe7cf46980ff
Flavio <fmontesano@gmail.com>
2023-12-22 16:15:59 +0100

    added device controller

--- controllers/device_controller.py
+++ controllers/device_controller.py
@@ -1,3 +1,7 @@
+import fcntl
+import struct
+from socket import socket
+
 from fastapi import FastAPI, APIRouter
 from fastapi.encoders import jsonable_encoder
 from starlette import status
@@ -11,8 +15,13 @@ class DeviceController:
         self.router.add_api_route(f"/{self.name}/ping", self.ping, methods=["GET"])
         app.include_router(self.router)
 
+    def getHwAddr(self, ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', ifname[:15]))
+        return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+
     def ping(self):
         return JSONResponse(
             status_code=status.HTTP_200_OK,
-            content=jsonable_encoder({"result": "Success", "status": "online"})
+            content=jsonable_encoder({"result": "Success", "status": "online", "device_unique_id" : self.getHwAddr("wlan0")})
         )
