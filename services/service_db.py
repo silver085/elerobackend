aaa4d96d1fc45f6e14fa476ff00f3fb7f000a98c
Flavio <fmontesano@gmail.com>
2023-12-28 14:43:58 +0100

    session db

--- services/service_db.py
+++ services/service_db.py
@@ -1,8 +1,5 @@
-from contextlib import contextmanager
-
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.exc import PendingRollbackError
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session
 
 from config.db import db_config
 import sys
@@ -19,19 +16,12 @@ class DBService:
         self.session = Session(self.engine)
 
     def execute_statement(self, statement):
-        try:
-            self.session_scope()
-            res = self.session.execute(statement)
-            self.session_scope()
-            return res
-        except PendingRollbackError:
-            print(f"Failed on SQL: {statement}")
-            self.session.rollback()
+        return self.session.execute(statement)
 
     def execute_insert(self, statement):
         try:
             result = self.session.execute(statement=statement)
-            self.session_scope()
+            self.session.commit()
             return result.inserted_primary_key
         except RuntimeError as e:
             print(f"Error during execute_insert: {e}")
@@ -40,27 +30,8 @@ class DBService:
     def execute_update(self, statement):
         try:
             result = self.session.execute(statement=statement)
-            self.session_scope()
+            self.session.commit()
             return True
         except RuntimeError as e:
             print(f"Error during execute_insert: {e}")
             return False
-
-    @contextmanager
-    def session_scope(self):
-        self.engine = create_engine(
-            f"mysql+mysqldb://{db_config['user']}:{db_config['passwd']}@{db_config['host']}/{db_config['db']}",
-            pool_pre_ping=True)  # echo=True if needed to see background SQL
-        Session = sessionmaker(bind=self.engine)
-        session = Session()
-        try:
-            # this is where the "work" happens!
-            yield session
-            # always commit changes!
-            session.commit()
-        except:
-            # if any kind of exception occurs, rollback transaction
-            session.rollback()
-            raise
-        finally:
-            session.close()
