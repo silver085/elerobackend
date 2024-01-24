10cbb4fc4cf2471980bf6003c1f54d928fc0dab5
Flavio <fmontesano@gmail.com>
2024-01-24 14:56:06 +0100

    added hygromether

--- controllers/device_controller.py
+++ controllers/device_controller.py
@@ -2,11 +2,12 @@ import fcntl
 import socket
 import struct
 
-
 from fastapi import FastAPI, APIRouter
 from fastapi.encoders import jsonable_encoder
 from starlette import status
 from starlette.responses import JSONResponse
+import RPi.GPIO as GPIO
+import dht11
 
 
 class DeviceController:
@@ -14,6 +15,7 @@ class DeviceController:
         self.name = "device"
         self.router = APIRouter()
         self.router.add_api_route(f"/{self.name}/ping", self.ping, methods=["GET"])
+        self.router.add_api_route(f"/{self.name}/hygrometer", self.hygrometer, methods=["GET"])
         app.include_router(self.router)
 
     def getHwAddr(self, ifname):
@@ -24,5 +26,26 @@ class DeviceController:
     def ping(self):
         return JSONResponse(
             status_code=status.HTTP_200_OK,
-            content=jsonable_encoder({"result": "Success", "status": "online", "device_unique_id" : self.getHwAddr("wlan0")})
+            content=jsonable_encoder(
+                {"result": "Success", "status": "online", "device_unique_id": self.getHwAddr("wlan0")})
         )
+
+    def hygrometer(self):
+        instance = dht11.DHT11(pin=14)
+        result = instance.read()
+        if result.is_valid():
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+            temp = "Temperature: %-3.1f" % result.temperature
+            hum = "Humidity: %-3.1f" % result.humidity
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=jsonable_encoder(
+                    {"result": "Success", "temperature" : {"value" : temp, "unit" : "°C"}, "humidity" : {"value" : hum, "unit": "%%"}})
+            )
+        else:
+            print("Error: %d" % result.error_code)
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content=jsonable_encoder({"result": "error", "message": "Unable to read valid results from sensor."})
+            )
