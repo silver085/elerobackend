9d112d874b7cd803f4b592a3801aac0c110dbde6
Flavio <fmontesano@gmail.com>
2024-01-24 14:34:56 +0100

    added pin for light

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Integer, String, DateTime, Boolean
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
